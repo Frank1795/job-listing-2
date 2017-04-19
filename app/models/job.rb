b006b81115d5b3887c83b82bc280156729a52058
张帆 <zf1795@gmail.com>
2017-04-19 08:52:56 +0800

    step 7 upload resume

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,6 +3,11 @@ class Job < ApplicationRecord
   validates :wage_upper_bound, presence: true
   validates :wage_lower_bound, presence: true
   validates :wage_lower_bound, numericality: { greater_than: 0}
+
+  scope :published, -> { where(is_hidden: false) }
+  scope :recent, -> { order('created_at DESC') }
+  has_many :resumes
+
   def publish!
     self.is_hidden = false
     self.save
@@ -13,6 +18,4 @@ class Job < ApplicationRecord
     self.save
   end
 
-  scope :published, -> { where(is_hidden: false) }
-  scope :recent, -> { order('created_at DESC') }
 end
